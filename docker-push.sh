39b6b28e0a96232e59c73216925bda8714983d47
Sidney Amaral <sidney@teclatab.com.br>
2019-10-07 21:55:50 -0300

    ajustes

--- docker-push.sh
+++ docker-push.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
+echo Passo 1...
 if ! [[ $TRAVIS_BRANCH =~ ^(develop|release|master)$ ]]
   echo "Este script é para uso com branches develop, release ou master apenas, não $TRAVIS_BRANCH"
   exit 1
 fi
 
+echo Passo 2...
 if [[ $IMAGEM_DOCKER == '' ]]
   echo "Varivável IMAGEM_DOCKER não definida"
   exit 2
 fi
 
+echo Passo 3...
 if [[ $TRAVIS_BRANCH =~ ^(release|master)$ && $TRAVIS_TAG == '' ]]
   echo "Nenhuma tag associada"
   exit 3
 fi
 
+echo Passo 4...
 if [[ $TRAVIS_BRANCH =~ ^(develop|release)$ ]]
   TAG_DOCKER=$IMAGEM_DOCKER:$TRAVIS_BRANCH
 else
   TAG_DOCKER=$IMAGEM_DOCKER:$TRAVIS_TAG
 fi
 
+echo Passo 5...
 docker build -t "$IMAGEM_DOCKER" .
 docker tag "$IMAGEM_DOCKER" "$TAG_DOCKER"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker push "$TAG_DOCKER"
+
+echo Fim

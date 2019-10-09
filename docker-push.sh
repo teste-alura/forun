3297a9e4ea24df60bbcb417c01ad87e1456ba635
Sidney Amaral <sidney@teclatab.com.br>
2019-10-09 18:04:11 -0300

    ajustes

--- docker-push.sh
+++ docker-push.sh
@@ -1,47 +1,70 @@
 #!/bin/bash
+
+terminarEmCasoDeErro () {
+  if (( $1 != 0 )); then exit $1; fi
+}
+
+COMMIT_BRANCHES=`git branch --contains $TRAVIS_COMMIT | tr -d \\n | tr -d \* 2> /dev/null` # branches (separados por espaço) que apontam para commit
+COMMIT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
+
+echo TRAVIS_COMMIT=[$TRAVIS_COMMIT]
 echo TRAVIS_BRANCH=[$TRAVIS_BRANCH]
-echo IMAGEM_DOCKER=[$IMAGEM_DOCKER]
+echo DOCKER_IMAGEM=[$DOCKER_IMAGEM]
 echo TRAVIS_TAG=[$TRAVIS_TAG]
 echo DOCKER_USERNAME=[$DOCKER_USERNAME]
+echo COMMIT_BRANCHES=[$COMMIT_BRANCHES]
+echo COMMIT_TAG=[$COMMIT_TAG]
+echo DOCKER_IMAGEM=[$DOCKER_IMAGEM]
 
-#echo Passo 1...
-#if ! [[ $TRAVIS_BRANCH =~ ^(develop|release|master)$ ]]; then
-#  echo "Erro: este script é para uso com branches develop, release ou master apenas, não $TRAVIS_BRANCH"
-#  exit 1
-#fi
-#
-#echo Passo 2...
-#if [[ $IMAGEM_DOCKER == '' ]]; then
-#  echo "Erro: varivável IMAGEM_DOCKER não definida"
-#  exit 2
-#fi
-#
-#echo Passo 3...
-#if [[ $TRAVIS_BRANCH =~ ^(release|master)$ && $TRAVIS_TAG == '' ]]; then
-#  echo "Erro: nenhuma tag associada ao commit"
-#  exit 3
-#fi
-#
-#echo Passo 4...
-#if [[ $TRAVIS_BRANCH =~ ^(develop|release)$ ]]; then
-#  TAG_DOCKER=$IMAGEM_DOCKER:$TRAVIS_BRANCH
-#else
-#  TAG_DOCKER=$IMAGEM_DOCKER:$TRAVIS_TAG
-#fi
+echo Passo 1...
 
-TAG_DOCKER=$IMAGEM_DOCKER:$TRAVIS_TAG
+if [[ $DOCKER_IMAGEM == '' ]]; then
+  echo "Erro: varivável DOCKER_IMAGEM não definida"
+  exit 1
+fi
+
+echo Passo 2...
+
+if [[ "$COMMIT_BRANCHES" =~ master ]]; then
+  COMMIT_BRANCH=master
+elif [[ "$COMMIT_BRANCHES" =~ release ]]; then
+  COMMIT_BRANCH=release
+elif [[ "$COMMIT_BRANCHES" =~ develop ]]; then
+  COMMIT_BRANCH=develop
+else
+  echo "Erro: este script é deploy dos branches develop, release ou master apenas"
+  exit 2
+fi
+
+echo Passo 3...
+if [[ $COMMIT_BRANCH =~ ^(release|master)$ && $COMMIT_TAG == '' ]]; then
+  echo "Erro: nenhuma tag associada ao commit para iniciar deploy de release/master"
+  exit 3
+fi
+
+echo Passo 4...
+if [[ $COMMIT_BRANCH == 'master' ]]; then
+  DOCKER_TAG=$DOCKER_IMAGEM:$COMMIT_TAG
+elif [[ $COMMIT_BRANCH == 'release' ]]; then
+  DOCKER_TAG=$DOCKER_IMAGEM:release-$COMMIT_TAG
+else
+  DOCKER_TAG=$DOCKER_IMAGEM:develop
+fi
 
 echo Passo 5...
-#docker build --build-arg BUILD_DIR="$TRAVIS_BUILD_DIR" -t "$IMAGEM_DOCKER" .
-docker build -t "$IMAGEM_DOCKER" .
+docker build -t "$DOCKER_IMAGEM" .
+terminarEmCasoDeErro $?
 
 echo Passo 6...
-docker tag "$IMAGEM_DOCKER" "$TAG_DOCKER"
+docker tag "$DOCKER_IMAGEM" "$DOCKER_TAG"
+terminarEmCasoDeErro $?
 
 echo Passo 7...
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+terminarEmCasoDeErro $?
 
 echo Passo 8...
-docker push "$TAG_DOCKER"
+docker push "$DOCKER_TAG"
+terminarEmCasoDeErro $?
 
 echo Fim

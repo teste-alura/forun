f75f186330bfcc52a5b67f28e4713435cc4d551a
Sidney Amaral <sidney@teclatab.com.br>
2019-10-09 22:29:15 -0300

    ajustes

--- docker-push.sh
+++ docker-push.sh
@@ -4,41 +4,51 @@ terminarEmCasoDeErro () {
   if (( $1 != 0 )); then exit $1; fi
 }
 
-COMMIT_LISTA_BRANCH=`git branch --contains $TRAVIS_COMMIT | tr -d '\n' | tr -d '*' | sed 's/(.*)//g' | xargs 2> /dev/null`
-COMMIT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
+#COMMIT_LISTA_BRANCH=`git branch --contains $TRAVIS_COMMIT | tr -d '\n' | tr -d '*' | sed 's/(.*)//g' | xargs 2> /dev/null`
+#COMMIT_TAG=`git describe --tags --exact-match $TRAVIS_COMMIT 2> /dev/null`
+
+COMMIT_BRANCHES_E_TAGS=`git ls-remote | grep $TRAVIS_COMMIT | sed 's/.*\///g' | tr '\n' ' ' 2> /dev/null`
+COMMIT_TAG=`echo $COMMIT_BRANCHES_E_TAGS | tr -s '[:space:]' '\n' | grep -i 'v[0-9]\+\.[0-9]\+\.[0-9]\+' | tr '\n' ' ' | xargs 2> /dev/null`
+
+if [[ $COMMIT_TAGS =~ ' ' ]]; then
+  echo "Erro: commit contém múltiplas TAGs ou o nome está fora do padrão v999.999.999"
+  exit 1
+fi
 
 echo TRAVIS_COMMIT=[$TRAVIS_COMMIT]
 echo TRAVIS_BRANCH=[$TRAVIS_BRANCH]
 echo DOCKER_IMAGEM=[$DOCKER_IMAGEM]
 echo TRAVIS_TAG=[$TRAVIS_TAG]
 echo DOCKER_USERNAME=[$DOCKER_USERNAME]
-echo COMMIT_LISTA_BRANCH=[$COMMIT_LISTA_BRANCH]
+echo COMMIT_BRANCHES_E_TAGS=[$COMMIT_BRANCHES_E_TAGS]
 echo COMMIT_TAG=[$COMMIT_TAG]
 
 echo Passo 1...
 
 if [[ $DOCKER_IMAGEM == '' ]]; then
   echo "Erro: varivável DOCKER_IMAGEM não definida"
-  exit 1
+  exit 2
 fi
 
 echo Passo 2...
 
-if [[ "$COMMIT_LISTA_BRANCH" =~ master ]]; then
+if [[ "$COMMIT_BRANCHES_E_TAGS" =~ master ]]; then
   COMMIT_BRANCH=master
-elif [[ "$COMMIT_LISTA_BRANCH" =~ release ]]; then
+elif [[ "$COMMIT_BRANCHES_E_TAGS" =~ release ]]; then
   COMMIT_BRANCH=release
-elif [[ "$COMMIT_LISTA_BRANCH" =~ develop ]]; then
+elif [[ "$COMMIT_BRANCHES_E_TAGS" =~ develop ]]; then
   COMMIT_BRANCH=develop
 else
   echo "Erro: este script é deploy dos branches develop, release ou master apenas"
-  exit 2
+  exit 3
 fi
+
+echo COMMIT_BRANCH=[$COMMIT_BRANCH]
 
 echo Passo 3...
 if [[ $COMMIT_BRANCH =~ ^(release|master)$ && $COMMIT_TAG == '' ]]; then
   echo "Erro: nenhuma tag associada ao commit para iniciar deploy de release/master"
-  exit 3
+  exit 4
 fi
 
 echo Passo 4...

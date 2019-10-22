6d390bdf10ae124512c388f2de90d3c25ffa2100
Sidney Amaral <sidney@teclatab.com.br>
2019-10-22 10:22:00 -0300

    ajuste sonarqube

--- src/main/java/br/com/alura/forum/controller/HelloController.java
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -1,13 +1,13 @@
 package br.com.alura.forum.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-	
-	@RequestMapping("/")
+
+	@GetMapping("/")
 	@ResponseBody
 	public String hello() {
 		return "Hello World!";
